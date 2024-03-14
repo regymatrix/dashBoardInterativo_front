e6f12c6e3aa424750adfc5c36b4edb3260797c65
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-14 00:21:19 -0300

    :sparkles: feat: implementada atualização aovivo 5 segundos

--- src/App.jsx
+++ src/App.jsx
@@ -31,188 +31,205 @@ export const App = () => {
   const [economicData, setEconomicData] = useState([]);
 
   useEffect(() => {
+    loadData();
+    setInterval(() => {
+      loadData();
+    }, 5000);
+  }, []);
+
+  function loadData() {
     api.get("/economia-por-robo").then((response) => {
       setTotalEconomiaPorRobo(response.data);
-      console.log(response.data);
     });
 
-    api.get("/produto-mais-economico").then((response) => {
-      setProdutoMaisEconomico(response.data);
-      console.log(response.data);
-    });
+    api.get("/produto-menos-economico").then((responseMenos) => {
+      setProdutoMenosEconomico(responseMenos.data);
 
-    api.get("/produto-menos-economico").then((response) => {
-      setProdutoMenosEconomico(response.data);
-      console.log(response.data);
+      api.get("/produto-mais-economico").then((responseMais) => {
+        setProdutoMaisEconomico(responseMais.data);
+
+        setEconomicData([
+          {
+            label: `Mais econômico: ${responseMais.data[0].nomeproduto}`,
+            maisEconomico: responseMais.data[0].economia,
+          },
+          {
+            label: `Menos econômico: ${responseMenos.data[0].nomeproduto}`,
+            menosEconomico: responseMenos.data[0].economia,
+          },
+        ]);
+      });
     });
 
     api.get("/top-10-produtos-economicos").then((response) => {
       setTop10ProdutosMaisEconomicos(response.data);
-      console.log(response.data);
     });
 
     api.get("/total-economia").then((response) => {
       setTotalEconomia(response.data);
-      console.log(response.data);
     });
-  }, []);
-
-  useEffect(() => {
-    if (produtoMaisEconomico.length === 0) return;
-    setEconomicData([
-      {
-        label: `Mais econômico: ${produtoMaisEconomico[0].nomeproduto}`,
-        maisEconomico: produtoMaisEconomico[0].economia,
-      },
-      {
-        label: `Menos econômico: ${produtoMenosEconomico[0].nomeproduto}`,
-        menosEconomico: produtoMenosEconomico[0].economia,
-      },
-    ]);
-  }, [produtoMenosEconomico]);
+  }
 
   return (
     <>
       <h1 className="title">Análise de relatórios benchmarking</h1>
 
-      <div className="App">
-        <div className="dataCard revenueCard">
-          <Bar
-            data={{
-              labels: totalEconomiaPorRobo.map((data) => data.codigoRobo),
-              datasets: [
-                {
-                  label: "Total de Economia",
-                  data: totalEconomiaPorRobo.map((data) => data.economiaTotal),
-                  backgroundColor: "#064FF0",
-                  borderColor: "#064FF0",
-                  borderRadius: 5,
+      {totalEconomiaPorRobo &&
+      totalEconomicData &&
+      top10ProdutosMaisEconomicos &&
+      economicData &&
+      totalEconomia &&
+      produtoMaisEconomico &&
+      produtoMenosEconomico ? (
+        <div className="App">
+          <div className="dataCard revenueCard">
+            <Bar
+              data={{
+                labels: totalEconomiaPorRobo.map((data) => data.codigoRobo),
+                datasets: [
+                  {
+                    label: "Total de Economia",
+                    data: totalEconomiaPorRobo.map(
+                      (data) => data.economiaTotal
+                    ),
+                    backgroundColor: "#064FF0",
+                    borderColor: "#064FF0",
+                    borderRadius: 5,
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
                 },
-              ],
-            }}
-            options={{
-              elements: {
-                line: {
-                  tension: 0.5,
+                plugins: {
+                  title: {
+                    text: "Total de economia por robô",
+                  },
                 },
-              },
-              plugins: {
-                title: {
-                  text: "Total de economia por robô",
-                },
-              },
-            }}
-          />
-        </div>
+              }}
+            />
+          </div>
 
-        <div className="dataCard totalCard">
-          <Bar
-            data={{
-              labels: totalEconomia.map((data) => data.label),
-              datasets: [
-                {
-                  fill: false,
-                  label: "Total de Economia",
-                  data: totalEconomia.map((data) => data.revenue),
-                  backgroundColor: "#064FF0",
-                  borderColor: "#064FF0",
-                  borderRadius: 5,
+          <div className="dataCard totalCard">
+            <Bar
+              data={{
+                labels: totalEconomia.map((data) => data.label),
+                datasets: [
+                  {
+                    fill: false,
+                    label: "Total de Economia",
+                    data: totalEconomia.map((data) => data.revenue),
+                    backgroundColor: "#064FF0",
+                    borderColor: "#064FF0",
+                    borderRadius: 5,
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
                 },
-              ],
-            }}
-            options={{
-              elements: {
-                line: {
-                  tension: 0.5,
+                plugins: {
+                  title: {
+                    text: "Total de economia realizada",
+                  },
                 },
-              },
-              plugins: {
-                title: {
-                  text: "Total de economia realizada",
-                },
-              },
-            }}
-          />
-        </div>
+              }}
+            />
+          </div>
 
-        <div className="dataCard customerCard">
-          <Bar
-            data={{
-              labels: economicData.map((data) => data.label),
-              datasets: [
-                {
-                  label: "mais econômico",
-                  data: economicData.map((data) => data.maisEconomico),
-                  backgroundColor: ["rgba(14, 209, 0, 0.8)"],
-                  borderRadius: 5,
-                },
-                {
-                  label: "menos econômico",
-                  data: economicData.map((data) => data.menosEconomico),
-                  backgroundColor: ["rgba(255, 49, 49, 0.8)"],
-                  borderRadius: 5,
-                },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  text: "Produto mais e menos econômico",
-                },
-              },
-            }}
-          />
-        </div>
-
-        <div className="dataCard categoryCard">
-          <Pie
-            data={{
-              labels: top10ProdutosMaisEconomicos.map(
-                (data) => data.nomeproduto
-              ),
-              datasets: [
-                {
-                  label: "Economia",
-                  data: top10ProdutosMaisEconomicos.map(
-                    (data) => data.economia
-                  ),
-                  backgroundColor: [
-                    "rgba(43, 63, 229, 0.8)",
-                    "rgba(250, 192, 19, 0.8)",
-                    "rgba(253, 135, 135, 0.8)",
-                    "rgba(135, 253, 157, 0.8)",
-                    "rgba(135, 241, 253, 0.8)",
-                    "rgba(237, 135, 253, 0.844)",
-                    "rgba(253, 135, 178, 0.844)",
-                    "rgba(170, 170, 170, 0.844)",
-                    "rgba(255, 0, 0, 0.844)",
-                    "rgba(0, 0, 0, 0.844)",
+          <div className="dataCard customerCard">
+            {economicData.length > 0 && (
+              <Bar
+                data={{
+                  labels: [
+                    economicData[0].label.split(":")[1],
+                    economicData[1].label.split(":")[1],
                   ],
-                  borderColor: [
-                    "rgba(43, 63, 229, 0.8)",
-                    "rgba(250, 192, 19, 0.8)",
-                    "rgba(253, 135, 135, 0.8)",
-                    "rgba(135, 253, 157, 0.8)",
-                    "rgba(135, 241, 253, 0.8)",
-                    "rgba(237, 135, 253, 0.844)",
-                    "rgba(253, 135, 178, 0.844)",
-                    "rgba(170, 170, 170, 0.844)",
-                    "rgba(255, 0, 0, 0.844)",
-                    "rgba(0, 0, 0, 0.844)",
+
+                  datasets: [
+                    {
+                      label: "mais econômico",
+                      data: economicData.map((data) => data.maisEconomico),
+                      backgroundColor: ["rgba(14, 209, 0, 0.8)"],
+                      borderRadius: 5,
+                    },
+                    {
+                      label: "menos econômico",
+                      data: economicData.map((data) => data.menosEconomico),
+                      backgroundColor: ["rgba(255, 49, 49, 0.8)"],
+                      borderRadius: 5,
+                    },
                   ],
+                }}
+                options={{
+                  plugins: {
+                    title: {
+                      text: "Produto mais e menos econômico",
+                    },
+                  },
+                }}
+              />
+            )}
+          </div>
+
+          <div className="dataCard categoryCard">
+            <Pie
+              data={{
+                labels: top10ProdutosMaisEconomicos.map(
+                  (data) => data.nomeproduto
+                ),
+                datasets: [
+                  {
+                    label: "Economia",
+                    data: top10ProdutosMaisEconomicos.map(
+                      (data) => data.economia
+                    ),
+                    backgroundColor: [
+                      "rgba(43, 63, 229, 0.8)",
+                      "rgba(250, 192, 19, 0.8)",
+                      "rgba(253, 135, 135, 0.8)",
+                      "rgba(135, 253, 157, 0.8)",
+                      "rgba(135, 241, 253, 0.8)",
+                      "rgba(237, 135, 253, 0.844)",
+                      "rgba(253, 135, 178, 0.844)",
+                      "rgba(170, 170, 170, 0.844)",
+                      "rgba(255, 0, 0, 0.844)",
+                      "rgba(0, 0, 0, 0.844)",
+                    ],
+                    borderColor: [
+                      "rgba(43, 63, 229, 0.8)",
+                      "rgba(250, 192, 19, 0.8)",
+                      "rgba(253, 135, 135, 0.8)",
+                      "rgba(135, 253, 157, 0.8)",
+                      "rgba(135, 241, 253, 0.8)",
+                      "rgba(237, 135, 253, 0.844)",
+                      "rgba(253, 135, 178, 0.844)",
+                      "rgba(170, 170, 170, 0.844)",
+                      "rgba(255, 0, 0, 0.844)",
+                      "rgba(0, 0, 0, 0.844)",
+                    ],
+                  },
+                ],
+              }}
+              options={{
+                plugins: {
+                  title: {
+                    text: "Top 10 produtos mais econômicos",
+                  },
                 },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  text: "Top 10 produtos mais econômicos",
-                },
-              },
-            }}
-          />
+              }}
+            />
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
     </>
   );
 };
