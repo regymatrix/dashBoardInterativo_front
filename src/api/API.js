b04b28484c217411ea1f7c559a80070356e15519
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-13 23:05:58 -0300

    att endpoint api

--- src/api/API.js
+++ src/api/API.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://regymatrix-001-site1.ktempurl.com/api/Logs",
-  auth: {
-    username: "11164448",
-    password: "60-dayfreetrial",
-  },
+  baseURL: "http:/  /gestaomargi-001-site8.gtempurl.com/api/Logs",
 });
