80e95a6e328bc1233c86fa2ccf9e9911e6d872a5
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-14 00:21:57 -0300

    :boom: fix: ajustando uri api

--- src/api/API.js
+++ src/api/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http:/  /gestaomargi-001-site8.gtempurl.com/api/Logs",
+  baseURL: "http://gestaomargi-001-site8.gtempurl.com/api/Logs",
 });
